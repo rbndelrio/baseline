160f3c88db464f7f2cd0f2f43c1700ea93745e38
Ruben Del Rio <rdelrio@pushhere.com>
2015-11-22 04:21:06 -0500

    Spaces < Tabs

--- src/index.php
+++ src/index.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="msapplication-tap-highlight" content="no">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="msapplication-tap-highlight" content="no">
 	<meta name="content-language" content="en_US">
-    <meta name="theme-color" content="#02528a">
+	<meta name="theme-color" content="#02528a">
 	<meta name="author" content="Push.">
 	<meta name="web_author" content="Push.">
 	<meta name="copyright" content="Push.">
 
-    <title></title>
+	<title></title>
 	<meta name="description" content="">
 	<meta name="twitter:description" content="" />
-    <meta property="og:title" content="" />
-    <meta property="og:description" content="" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="" />
-    <meta property="og:image" content="" />
+	<meta property="og:title" content="" />
+	<meta property="og:description" content="" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="" />
+	<meta property="og:image" content="" />
 	<meta property="og:locale" content="en_US" />
 
 	<link rel="apple-touch-icon" href="img/touch-icon.png">
@@ -29,8 +29,8 @@
 	<link rel="stylesheet" type="text/css" href="/css/style.css"/>
 
 	<!--[if lt IE 9]>
-	  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
-	  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv-printshiv.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
 </head>
 <body class="fp-render">
